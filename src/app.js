13fcce6e6abac0753bea35c03660f4e77d1722f0
Ivan Shirobokov <spirit-1993@list.ru>
2018-08-19 11:32:15 +0100

    Тестирует настройку прекоммит хука

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import polyfill from './polyfill';
+
+const app = (window) => {
+  const { document, Element } = window;
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = polyfill;
+  }
+  function init() {
+    document.documentElement.classList.add('togglr');
+  }
+  window.addEventListener('DOMContentloaded', init);
+
+  function ftogglr(event) {
+    const { target } = event;
+    const controlItem = target.closest('[data-toggl-target]');
+    const selectorValue = controlItem.dataset.togglTarget;
+    const selectorValueEl = document.querySelectorAll(selectorValue);
+
+
+    if (controlItem.hasAttribute('data-togglr-toggle')) {
+      const toggleClassValue = controlItem.dataset.togglrToggle;
+      selectorValueEl.forEach((element) => {
+        element.classList.toggle(toggleClassValue);
+      });
+    } else if (controlItem.hasAttribute('data-togglr-add')) {
+      const addClassValue = controlItem.dataset.togglrAdd;
+      selectorValueEl.forEach((element) => {
+        element.classList.add(addClassValue);
+      });
+    } else if (controlItem.hasAttribute('data-togglr-remove')) {
+      const removeClassValue = controlItem.dataset.togglrRemove;
+      selectorValueEl.forEach((element) => {
+        element.classList.remove(removeClassValue);
+      });
+    } else if (controlItem.hasAttribute('data-togglr-exclusive')) {
+      const exclusiveValue = controlItem.dataset.togglrExclusive;
+      selectorValueEl.forEach((element) => {
+        element.classList.add(exclusiveValue);
+        element.nextElementSibling.classList.remove(exclusiveValue);
+        element.previousElementSibling.classList.remove(exclusiveValue);
+      });
+    } else if (controlItem.hasAttribute('data-togglr-exclusiveAdd')) {
+      const exclusiveAddValue = controlItem.dataset.togglrExclusiveadd;
+      selectorValueEl.forEach((element) => {
+        element.classList.add(exclusiveAddValue);
+        element.nextElementSibling.classList.add(exclusiveAddValue);
+        element.previousElementSibling.classList.add(exclusiveAddValue);
+      });
+    }
+  }
+  document.addEventListener('click', ftogglr);
+  console.log(q);
+};
+export default app;
